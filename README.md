a84579de2da1ac017e00fd2b3fdc2958ebaf2772
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-04-07 14:43:14 -0600

    crud working great

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FirestoreWithAngular
-Test project for setting up [Firestore](https://firebase.google.com/docs/firestore) with Angular. 
+Docs: Test project for setting up [Firestore](https://firebase.google.com/docs/firestore) with Angular. 
+
+To run: 
+1. Set the `firebaseConfig` variable, inside the `app.module.ts` file, to your project's settings.
+2. When making the CRUD calls, make sure you're hitting the right path to your db.
--- firestoreWithAngular/src/app/accounts/accounts.component.ts
+++ firestoreWithAngular/src/app/accounts/accounts.component.ts
@@ -1,31 +1,89 @@
-import { Component } from "@angular/core";
-import { AngularFirestoreCollection, AngularFirestore } from "@angular/fire/compat/firestore";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AngularFirestoreCollection, AngularFirestore, DocumentReference } from "@angular/fire/compat/firestore";
+import { Subscription } from "rxjs";
 
 export interface ITask {
-  TaskName: string
-  Due: string
-  Ends: string
-  Status: string
+  Name: string
+  DueDate: string
+  ExpireDate: string
+  CurrentStatus: string
+  ID: string; // id of document in firestore
+}
+
+export interface IUpdateTask {
+  Name?: string
+  DueDate?: string
+  ExpireDate?: string
+  CurrentStatus?: string
+  ID?: string; // id of document in firestore
 }
 
 @Component({
   selector: 'accounts',
   templateUrl: './accounts.component.html',
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit, OnDestroy {
   public tasksByLocation: ITask[] = [];
-  private tasksDB: AngularFirestoreCollection<ITask[]>;
 
-  constructor(private afs: AngularFirestore) {
-    const orgId = 'bQ9CHs0Zc2xZt4zYVumI';
-    const locationId = 'R3hdMx59LpEIlinlL45p';
-    this.tasksDB = this.afs.collection<ITask[]>('accounts').doc(orgId).collection('locations').doc(locationId).collection('tasks');
+  private orgId = '96';
+  private locationId = '107';
+  private subscriptions = new Subscription();
+
+  constructor(private afs: AngularFirestore) {}
+  
+  public ngOnInit(): void {
     this.setupLocationDbListener();
   }
 
-  private setupLocationDbListener(): void {
-    this.tasksDB.valueChanges().subscribe((response: ITask[] | any) => {
-      this.tasksByLocation = response;
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  public createTask(): void {
+    const tasksCollection: AngularFirestoreCollection<ITask> = this.getTasksCollection();
+    const newTask: ITask = {
+      Name: "My New Task",
+      DueDate: "2022-08-04",
+      ExpireDate: "2022-03-09",
+      CurrentStatus: "Open",
+      ID: null
+    }
+
+    tasksCollection.add(newTask)
+    .then((response: DocumentReference) => {
+      this.updateTask(response.id, { ID: response.id })
     })
+  }
+
+  public updateTask(taskID: string, taskDataToUpdate: IUpdateTask): void {
+    const tasksCollection: AngularFirestoreCollection<ITask> = this.getTasksCollection();
+    tasksCollection
+      .doc(taskID)
+      .set(taskDataToUpdate as ITask, { merge: true }); // Change some properties without passing up the entire object. 
+  }
+
+  public deleteTask(taskID: string): void {
+    const tasksCollection: AngularFirestoreCollection<ITask> = this.getTasksCollection();
+    tasksCollection.doc(taskID).delete();
+  }
+
+  private setupLocationDbListener(): void {
+    const tasksCollection: AngularFirestoreCollection<ITask> = this.getTasksCollection();
+    const tasksDBSub = tasksCollection
+      .valueChanges()
+      .subscribe((response: ITask[] | any) => {
+        this.tasksByLocation = response;
+      })
+
+    this.subscriptions.add(tasksDBSub);
+  }
+
+  private getTasksCollection(): AngularFirestoreCollection<ITask> {
+    return this.afs
+      .collection('accounts')
+      .doc(this.orgId)
+      .collection('locations')
+      .doc(this.locationId)
+      .collection('tasks')
   }
 }
